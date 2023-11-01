def62dc7eebb208cc13ddcd01fc9ed972428e312
Anass <146187264+bokettoo@users.noreply.github.com>
2023-11-01 12:58:05 +0100

    Add files via upload

---
+++ password.py
@@ -0,0 +1,10 @@
+for i in range (0,3):
+    password=input('enter the password')
+    if password=="DEV@2023":
+        print("bojour")
+        break
+movie=(input("you run out of tries, please answer the following question. What is your favourite movie:   "))
+if movie=="The Shawshank Redemption (1994)":
+    print("bounjour")
+else:
+    print("bye!")
